88197ea5a2748a7b7f76840fec1f2e5655b61ba4
t-h-e-silence <liliyaaltemirova11@gmail.com>
2020-02-01 00:21:49 +0200

    Test

---
+++ core/src/main/java/com/alevel/java/nix/testshomework/MinAndMax.java
@@ -0,0 +1,36 @@
+package com.alevel.java.nix.testshomework;
+
+public class MinAndMax {
+
+    int [] arr;
+
+
+    public MinAndMax(int[] arr) {
+        this.arr=arr;
+    }
+
+    public int Min() {
+
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min> arr[i]) {
+                min = arr[i];
+            }
+
+        }
+        return min;
+    }
+
+    public int Max() {
+
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (max< arr[i]) {
+               max = arr[i];
+
+            }
+
+        }
+        return max;
+    }
+}
