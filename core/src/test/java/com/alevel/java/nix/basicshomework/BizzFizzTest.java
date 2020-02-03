7f656ba6d326979351031f83aeb2de9dc5e70f3b
t-h-e-silence <liliyaaltemirova11@gmail.com>
2020-02-04 01:05:14 +0200

    TestHomework

---
+++ core/src/test/java/com/alevel/java/nix/basicshomework/BizzFizzTest.java
@@ -0,0 +1,17 @@
+package com.alevel.java.nix.basicshomework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BizzFizzTest {
+
+    @Test
+    void leftRight() {
+
+    }
+
+    @Test
+    void rightLeft() {
+    }
+}
