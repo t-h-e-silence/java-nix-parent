b9f2ba75c55b857a319858598aa22c321bbb1593
t-h-e-silence <liliyaaltemirova11@gmail.com>
2020-02-04 23:31:08 +0200

    TestHomework

---
+++ core/src/test/java/com/alevel/java/nix/basicshomework/LongnumsTest.java
@@ -0,0 +1,28 @@
+package com.alevel.java.nix.basicshomework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongnumsTest {
+    @Test
+    void numsOfBits() {
+        Longnums l = new Longnums();
+        int actual = l.numsOfBits();
+        assertEquals(0, actual);
+        Longnums l1 = new Longnums(1_111_122_222_333_33L);
+        actual = l1.numsOfBits();
+        assertEquals(22, actual);
+        Longnums l2 = new Longnums(-1_111_122_222_333_33L);
+        actual = l2.numsOfBits();
+        assertEquals(43, actual);
+        Longnums l3 = new Longnums(-6_489_945_7176_598_302L);
+        actual = l3.numsOfBits();
+        assertEquals(36, actual);
+
+        Longnums l4 = new Longnums(6_489_945_7176_598_302L);
+        actual = l4.numsOfBits();
+        assertEquals(28, actual);
+    }
+
+}
