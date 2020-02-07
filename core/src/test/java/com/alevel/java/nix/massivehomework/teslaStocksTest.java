07b4ef97b523de8f5eb3b3dfb8dc23ba69340fe2
t-h-e-silence <liliyaaltemirova11@gmail.com>
2020-02-07 22:40:45 +0200

    BasicHomework

---
+++ core/src/test/java/com/alevel/java/nix/massivehomework/teslaStocksTest.java
@@ -0,0 +1,21 @@
+package com.alevel.java.nix.massivehomework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class teslaStocksTest {
+
+    @Test
+    void getTheDay() {
+        double[] mas = {12.3, 7.8, 6.0, 14, 12.3, 105.5, 34.5};
+
+        TeslaStocks t = new TeslaStocks(mas);
+        String ans = t.getTheDay();
+        assertEquals("День покупки " + 3 + " День продажи " + 6 + " Разница в цене " + 99.5, ans);
+        System.out.println(ans);
+        TeslaStocks t1 = new TeslaStocks();
+        String ans1 = t1.getTheDay();
+        assertEquals("День покупки " + 1 + " День продажи " + 7 + " Разница в цене " + 6.0, ans1);
+    }
+}
