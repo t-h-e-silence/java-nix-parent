d791530b67468da2082939cded13c848cd7b61cf
t-h-e-silence <liliyaaltemirova11@gmail.com>
2020-02-18 20:20:22 +0200

    Level 3 Task 1

--- module1/src/main/java/level3/LiveGame.java
+++ module1/src/main/java/level3/LiveGame.java
@@ -3,27 +3,28 @@ package level3;
 public class LiveGame {
     int rows;
     int colls;
-    int[][] arr;
+    boolean[][] arr;
 
-    LiveGame(int[][] arr) {
+    LiveGame(boolean[][] arr) {
         this.arr = arr;
         rows = this.arr.length;
         colls = this.arr[0].length;
     }
 
     LiveGame() {
-        this.arr = new int[][]{{0, 1, 0, 0, 1}, {1, 0, 1, 0, 0}, {0, 1, 1, 0, 0}, {1, 0, 0, 1, 1}, {1, 0, 0, 1, 0}};
+        this.arr = new boolean[][]{{false, true, false, false, true}, {true, false, true, false, false},
+                {false, true, true, false, false}, {true, false,false,true,true},{true, false,false,true,false}};
         rows = this.arr.length;
         colls = this.arr[0].length;
     }
 
 
-    public int[][] nextState() {
-        int[][] newarr = new int[rows + 2][colls + 2];
+    public boolean[][] nextState() {
+        boolean[][] newarr = new boolean[rows + 2][colls + 2];
 
         int rowLength = Math.min(rows, arr.length);
         for (int n = 0; n < rowLength; n++) {
-            int[] row = arr[n];
+            boolean[] row = arr[n];
             if (row == null) continue;
             int columnLenght = Math.min(colls, row.length);
             System.arraycopy(row, 0, newarr[n + 1], 1, columnLenght);
@@ -31,36 +32,36 @@ public class LiveGame {
         for (int i = 1; i < newarr.length - 1; i++) {
             for (int j = 1; j < newarr[i].length - 1; j++) {
                 int alive = 0;
-                if (newarr[i][j + 1] != 0) {
+                if (newarr[i][j + 1] != false) {
                     alive++;
                 }
-                if (newarr[i][j - 1] != 0) {
+                if (newarr[i][j - 1] != false) {
                     alive++;
                 }
-                if (newarr[i + 1][j] != 0) {
+                if (newarr[i + 1][j] != false) {
                     alive++;
                 }
-                if (newarr[i - 1][j] != 0) {
+                if (newarr[i - 1][j] != false) {
                     alive++;
                 }
-                if (newarr[i + 1][j + 1] != 0) {
+                if (newarr[i + 1][j + 1] != false) {
                     alive++;
                 }
-                if (newarr[i - 1][j - 1] != 0) {
+                if (newarr[i - 1][j - 1] != false) {
                     alive++;
                 }
-                if (newarr[i - 1][j + 1] != 0) {
+                if (newarr[i - 1][j + 1] != false) {
                     alive++;
                 }
-                if (newarr[i + 1][j - 1] != 0) {
+                if (newarr[i + 1][j - 1] != false) {
                     alive++;
                 }
-                if (newarr[i][j] == 1) {
+                if (newarr[i][j] == true) {
                     if (alive < 2 || alive > 3)
-                        arr[i - 1][j - 1] = 0;//die
+                        arr[i - 1][j - 1] = false;//die
                 }
-                if (newarr[i][j] == 0 && alive == 3) {
-                    arr[i - 1][j - 1] = 1;
+                if (newarr[i][j] == false && alive == 3) {
+                    arr[i - 1][j - 1] = true;
                 }
             }
         }
--- module1/src/test/java/level3/LiveGameTest.java
+++ module1/src/test/java/level3/LiveGameTest.java
@@ -12,11 +12,13 @@ class LiveGameTest {
     @Test
     void nextState() {
 
-        int[][] arr = {{0, 1, 0, 0, 1}, {1, 0, 1, 0, 0}, {0, 1, 1, 0, 0}, {1, 0, 0, 1, 1}, {1, 0, 0, 1, 0}};
-        int[][] exparr = {{0, 1, 0, 0, 0}, {1, 0, 1, 1, 0}, {1, 0, 1, 0, 0}, {1, 0, 0, 1, 1}, {0, 0, 0, 1, 1}};
+        boolean[][] arr ={{false, true, false, false, true}, {true, false, true, false, false},
+                {false, true, true, false, false}, {true, false,false,true,true},{true, false,false,true,false}};
+        boolean[][] exparr = {{false, true, false, false, false}, {true, false, true,true, false},
+                {true, false, true, false, false}, {true, false, false, true,true}, {false, false, false, true,true}};
         String expstr = Arrays.deepToString(exparr);
         LiveGame l = new LiveGame(arr);
-        int[][] newarr = l.nextState();
+        boolean[][] newarr = l.nextState();
         String newstr = Arrays.deepToString(newarr);
         assertEquals(expstr, newstr);
     }
